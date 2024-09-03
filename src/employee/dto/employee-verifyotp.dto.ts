a2cf0508171676764c5b63b261c179b7d3ecccdf
rioanggoro <rioanggoro37@gmail.com>
2024-09-03 23:13:11 +0700

    fix: changes message in verify token

--- src/employee/dto/employee-verifyotp.dto.ts
+++ src/employee/dto/employee-verifyotp.dto.ts
@@ -1,10 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class EmployeeVerifyOtpDto {
-  @IsNotEmpty()
-  @IsString()
   email: string;
-
-  @IsNotEmpty()
-  @IsNumber()
   otp: number;
 }
